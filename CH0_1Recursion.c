136cbabd635edaa8b1a3444b5d121639133c435b
Mathswesh <agniveshpatel1910@gmail.com>
2025-01-17 16:25:34 +0530

    recursion

---
+++ CH0_1Recursion.c
@@ -0,0 +1,31 @@
+/*
+    Author : Agnivesh Patel
+    Subject : Recursion in Factorial Programe
+    Date : 17/jan/2025
+*/
+#include<stdio.h>
+int count = 0;
+
+int factorial(int num){
+    // base case
+    if(num == 0){
+        return 1 ;
+    }
+    // recursive case
+    else
+    {
+        return num*factorial(num-1);
+    }
+}
+
+int main() {
+    int num;
+    printf("\nEnter the Number : ");
+    scanf("%d",&num);
+    printf("factorial of %d is %d", num , factorial(num));
+    return 0;
+}
+
+
+//in 1st call  -->>>> 
+//in 2nd call
